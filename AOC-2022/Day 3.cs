5686c518e84ccccbe5f188d0358c6a953f1a0ce5
Matt <mattogascar@gmail.com>
2022-12-05 17:21:44 -0600

    added day 3 and day 4

--- AOC-2022/Day 3.cs
+++ AOC-2022/Day 3.cs
@@ -6,17 +6,41 @@ public class Day3
     {
         var inputStream = new StreamReader("inputs/03.txt");
         var line = inputStream.ReadLine();
+        int prioritySum = 0;
         while (!string.IsNullOrWhiteSpace(line))
         {
+            var compartment1 = line.Substring(0, line.Length / 2);
+            var compartment2 = line.Substring(line.Length / 2, line.Length / 2);
+            var matchingItem = compartment1.Intersect(compartment2).FirstOrDefault();
+
+            prioritySum += GetPriority(matchingItem);
+            line = inputStream.ReadLine();
         }
+        Console.WriteLine(prioritySum);
     }
 
     public static void Part2()
     {
         var inputStream = new StreamReader("inputs/03.txt");
-        var line = inputStream.ReadLine();
-        while (!string.IsNullOrWhiteSpace(line))
+        bool loop = true;
+        var prioritySum = 0;
+        while (loop)
         {
+            var line1 = inputStream.ReadLine();
+            var line2 = inputStream.ReadLine();
+            var line3 = inputStream.ReadLine();
+            var matchingItem = line1.Intersect(line2).Intersect(line3).FirstOrDefault();
+            prioritySum += GetPriority(matchingItem);
+            if (inputStream.Peek() == -1) loop = false;
         }
+
+        Console.WriteLine(prioritySum);
+    }
+
+    static int GetPriority(char item)
+    {
+        if (item >= 65 && item <= 90) return item - 65 + 27;
+        else if (item >= 97 && item <= 122) return item - 96;
+        return 0;
     }
 }
--- AOC-2022/Day 4.cs
+++ AOC-2022/Day 4.cs
@@ -6,17 +6,56 @@ public class Day4
     {
         var inputStream = new StreamReader("inputs/04.txt");
         var line = inputStream.ReadLine();
+        var containedPairCount = 0;
         while (!string.IsNullOrWhiteSpace(line))
         {
+            var pair = line.Split(',');
+            var elf1 = pair[0].Split('-');
+            var elf2 = pair[1].Split('-');
+            int.TryParse(elf1[0], out int elf1LB);
+            int.TryParse(elf1[1], out int elf1UB);
+            int.TryParse(elf2[0], out int elf2LB);
+            int.TryParse(elf2[1], out int elf2UB);
+
+            bool check1=false, check2 = false, check3 = false, check4 = false;
+            if (elf1LB <= elf2LB && elf2LB <= elf1UB) check1 = true;
+            if (elf1LB <= elf2UB && elf2UB <= elf1UB) check2 = true;
+            if (elf2LB <= elf1LB && elf1LB <= elf2UB) check3 = true;
+            if (elf2LB <= elf1UB && elf1UB <= elf2UB) check4 = true;
+
+            if ((check1 && check2) || (check3 && check4)) containedPairCount++;
+            line = inputStream.ReadLine();
         }
+        Console.Write(containedPairCount);
     }
+
 
     public static void Part2()
     {
         var inputStream = new StreamReader("inputs/04.txt");
         var line = inputStream.ReadLine();
+        var containedPairCount = 0;
         while (!string.IsNullOrWhiteSpace(line))
         {
+            var pair = line.Split(',');
+            var elf1 = pair[0].Split('-');
+            var elf2 = pair[1].Split('-');
+            int.TryParse(elf1[0], out int elf1LB);
+            int.TryParse(elf1[1], out int elf1UB);
+            int.TryParse(elf2[0], out int elf2LB);
+            int.TryParse(elf2[1], out int elf2UB);
+
+            bool check1 = false, check2 = false, check3 = false, check4 = false;
+            if (elf1LB <= elf2LB && elf2LB <= elf1UB) check1 = true;
+            if (elf1LB <= elf2UB && elf2UB <= elf1UB) check2 = true;
+            if (elf2LB <= elf1LB && elf1LB <= elf2UB) check3 = true;
+            if (elf2LB <= elf1UB && elf1UB <= elf2UB) check4 = true;
+
+            if (check1 || check2 || check3 || check4) containedPairCount++;
+            line = inputStream.ReadLine();
         }
+        Console.Write(containedPairCount);
     }
+
+
 }
